066f4b3eec64997ef6011d15e93287da96fa8461
sthippo <mash@brooklyndigital.net>
2019-04-19 08:28:39 -0400

    refresh command

--- poeprog.sh
+++ poeprog.sh
@@ -10,12 +10,20 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+COMMAND="$1"
 
-if [[ "$1" == "start" || "$1" == "stop" || "$1" == "status" || "$1" == "run"  || "$1" == "restart" ]]; then
+if [[ $COMMAND == "refresh" ]]; then
+    pushd "${DIR}"
+    git pull
+    popd
+    COMMAND="restart"
+fi
+
+if [[ $COMMAND == "start" || $COMMAND == "stop" || $COMMAND == "status" || $COMMAND == "run"  || $COMMAND == "restart" ]]; then
     source ${DIR}/bin/cantoserver.sh "$@" --site poetic_programming
 else
     echo
-    echo "Usage: ${0##*/} {start|stop|run|restart|status}"
+    echo "Usage: ${0##*/} {start|stop|run|restart|refresh|status}"
     echo
     exit 1
 fi
